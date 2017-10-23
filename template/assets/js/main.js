ad35eae58f723147b70adb5ab7c9e28fb54f6e8d
seunagbeye <agbeyeseun1@gmail.com>
2017-10-23 03:41:52 +0100

    created more pages, added images, responsiveness and categories

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -1,9 +1,17 @@
 
 // Adding steps
-let counter = 1
+let step = 1
 $('#step').on('click',function(element) {
 	element.preventDefault();
-	$(this).before("<fieldset class='form-group'><label>Step " + counter + "</label><input class = 'form-control'></input></fieldset>");
-	counter +=1
+	$(this).before("<fieldset class='form-group'><label>Step " + step + "</label><input class = 'form-control'></input></fieldset>");
+	step +=1
+	/* Act on the event */
+});
+
+let ingredient = 1
+$('#ingredient').on('click',function(element) {
+	element.preventDefault();
+	$(this).before("<fieldset class='form-group'><label>Ingredient " + ingredient + "</label><input class = 'form-control'></input></fieldset>");
+	ingredient +=1
 	/* Act on the event */
 });
