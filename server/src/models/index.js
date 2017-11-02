86cb7d86f96b300179ebd1c9e4adcdb0b6b344d5
seunagbeye <agbeyeseun1@gmail.com>
2017-11-02 12:53:22 +0100

    refactor for production

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -12,23 +12,22 @@ console.log(config)
 const db = {};
 console.log(env)
 let sequelize;
-if (config.environment === 'production') {
-  // sequelize = new Sequelize(process.env[config.use_env_variable]);
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,{
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-      dialectOption: {
-        ssl: true,
-        native: true
-      },
-      logging: true
-    }
-  );
-  console.log(sequelize, 'hello')
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  // sequelize = new Sequelize(
+  //   process.env.DB_NAME,
+  //   process.env.DB_USER,
+  //   process.env.DB_PASS,{
+  //     host: process.env.DB_HOST,
+  //     port: process.env.DB_PORT,
+  //     dialect: 'postgres',
+  //     dialectOption: {
+  //       ssl: true,
+  //       native: true
+  //     },
+  //     logging: true
+  //   }
+  // );
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
