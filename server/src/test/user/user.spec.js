2d5504b711f21a22b7af3b2aa7003f52efe2ab01
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-02-26 17:25:03 +0100

    e2e and coverage

--- server/src/test/user/user.spec.js
+++ server/src/test/user/user.spec.js
@@ -365,6 +365,7 @@ describe('Test For Users Routes', () => {
         .end((error, res) => {
           expect(res).to.have.status(200);
           expect(res.body.data).to.have.property('token');
+          console.log('>>>>>>>>>>>>>>>>>>', error)
           done();
         });
     });
