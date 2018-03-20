d7c95793799336d02a4a27353b8583167fb8ae49
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-03-20 10:32:11 +0100

    chore(feedback): lf feedback file names must match the classnames the file is exporting
    [Starts #155939374]

--- client/main/utils/requireAuthentication.jsx
+++ client/main/utils/requireAuthentication.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import checkAuth from '../utils/CheckAuth';
+import checkAuth from '../utils/checkAuth';
 
 /**
  * @description - Higher order component for forcing authentication
